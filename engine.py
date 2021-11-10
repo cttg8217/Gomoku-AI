e35426ac31aa0b0c9276c9e09da18084034e0150
CTTG8217 <cttg8217@gmail.com>
2021-11-10 15:16:49 +0900

    work

--- engine.py
+++ engine.py
@@ -2,8 +2,8 @@
 Game Engine For Gomoku
 """
 
-height = 8
-width = 8
+HEIGHT = 8
+WIDTH = 8
 
 WHITE = 1
 BLACK = -1
@@ -12,8 +12,8 @@ EMPTY = 0
 
 def initial_board():
     board = []
-    for i in range(height):
-        board.append([EMPTY] * width)
+    for i in range(HEIGHT):
+        board.append([EMPTY] * WIDTH)
     return board
 
 
@@ -36,14 +36,41 @@ def player(board):
     black_count = 0
     for row in board:
         for cell in row:
-            print(cell)
             if cell == WHITE:
                 white_count += 1
             elif cell == BLACK:
                 black_count += 1
-    print(white_count, black_count)
     if white_count == black_count:
         return WHITE
     if white_count == black_count+1:
         return BLACK
+
     raise Exception("Unable to Calculate Player: Invalid Board")
+
+
+def actions(board, interest_range):
+    cells_of_interest = []
+    for row in range(HEIGHT):
+        for col in range(WIDTH):
+            if board[row][col] != EMPTY:
+                for i in range(row-interest_range, row+interest_range+1):
+                    for j in range(col-interest_range, col+interest_range+1):
+                        if 0 <= i < HEIGHT and 0 <= j < WIDTH and board[i][j] == EMPTY:
+                            if (i, j) not in cells_of_interest:
+                                cells_of_interest.append((i, j))
+
+    return cells_of_interest
+
+
+def execute_action(board, action):
+    (row, col) = action
+    if board[row][col] != EMPTY:
+        raise Exception("Unable to Execute Action: Invalid Action")
+    board[row][col] = player(board)
+
+
+def cancel_last_action(board, action):
+    (row, col) = action
+    if board[row][col] == EMPTY:
+        raise Exception("Unable to Cancel Action: Action Did Not Exit")
+    board[row][col] = EMPTY
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ from engine import *
 
 board = initial_board()
 board[0][0] = WHITE
-board[0][1] = WHITE
-print(player(board))
+board[1][1] = BLACK
+print(actions(board, 1))
